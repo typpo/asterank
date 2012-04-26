c6422ed6d699e0372f8303a7cefc98835ec46e60
Ian <typppo@gmail.com>
2012-04-26 00:58:14 -0700

    asteroid class

--- util/lookup.py
+++ util/lookup.py
@@ -3,25 +3,32 @@ import sys
 import urllib
 from bs4 import BeautifulSoup
 
+# TODO  close approach data
+#       earth MOID
 
-def lookup(query):
-  r = Query(query)
 
-  data = {}
-  data['diameter_km'] = r.physicalParameter('diameter')
-  data['gm'] = r.physicalParameter('GM')
-  data['density'] = r.physicalParameter('bulk density')
+class Asteroid:
+  def __init__(self, name):
+    self.name = name
 
-  data['passage_jed'] = r.orbitalParameter('t')
-  data['perhilion_au'] = r.orbitalParameter('q')
-  data['semimajor_au'] = r.orbitalParameter('a')
-  data['period_days'] = r.orbitalParameter('period')
+  def load(self):
+    r = Query(self.name)
 
-  print data
+    self.data = {}
+    self.data['diameter_km'] = r.physicalParameter('diameter')
+    self.data['gm'] = r.physicalParameter('GM')
+    self.data['density'] = r.physicalParameter('bulk density')
+
+    self.data['passage_jed'] = r.orbitalParameter('t')
+    self.data['perhilion_au'] = r.orbitalParameter('q')
+    self.data['semimajor_au'] = r.orbitalParameter('a')
+    self.data['period_days'] = r.orbitalParameter('period')
+
+    print self.data
 
 class Query:
   def __init__(self, query):
-    src = urllib.urlopen('http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=%s' % query ).read()
+    src = urllib.urlopen('http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=%s;cad=1' % query ).read()
     self.soup = BeautifulSoup(src.replace('cellspacing="0"0', ''))
 
   def orbitalParameter(self, txt):
@@ -38,8 +45,12 @@ class Query:
       return float(el)
     return -1
 
+  def additionalInfoParameter(self, txt):
+    pass
+
 if __name__ == "__main__":
   if len(sys.argv) != 2:
     print 'usage: lookup <name>'
     sys.exit(1)
-  lookup(' '.join(sys.argv[1:]))
+  a = Asteroid(' '.join(sys.argv[1:]))
+  a.load()
