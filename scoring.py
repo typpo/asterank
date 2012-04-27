5b85229241d6fd0b1180401ba58f16bfb34b90d8
typpo <ian@typpo.us>
2012-04-27 19:10:32 -0400

    small calculations

--- scoring.py
+++ scoring.py
@@ -1,3 +1,7 @@
+#
+# Scoring function for asteroid objects
+#
+import math
 
 def closeness_weight(obj):
   s = (10-obj['moid']) * 3
@@ -8,8 +12,13 @@ def closeness_weight(obj):
   return s
 
 def price(obj):
-  # TODO
-  return 1
+  G = 6.67300e-11
+  gmass = obj['GM'] if isinstance(obj['GM'], basestring) else 1.47e21 * G
+  radius = obj['diameter'] if 'diameter' in obj else 5  # 5km radius by default
+  vol = 4/3 * math.pi * math.pow(radius, 3) # model as sphere
+  density = gmass / vol
+
+  return gmass
 
 def score(obj):
   return closeness_weight(obj) * price(obj)
