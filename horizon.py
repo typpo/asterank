fa56cee945ae8cc623b5c259540efecb9ac646c1
typpo <ian@typpo.us>
2012-04-27 20:09:15 -0400

    scoring tuning

--- horizon.py
+++ horizon.py
@@ -12,16 +12,19 @@ from pymongo import Connection
 DATA_PATH = 'data/fulldb.csv'
 
 def populateDb():
-  print 'Loading small body data...this may take a little bit'
+  print 'Loading small body data...this may take a while'
 
   conn = Connection('localhost', 27017)
   db = conn.asterank
   coll = db.asteroids
   coll.ensure_index('full_name', unique=True)
+  coll.ensure_index('score')
 
   reader = csv.DictReader(open(DATA_PATH), delimiter=',', quotechar='"')
   n = 0
   for row in reader:
+    if row['spec_T'] == '':
+      continue
     for key,val in row.items():
       # Clean up the input
       try:
@@ -32,12 +35,12 @@ def populateDb():
         row[key] = fv
 
     # compute score
-    row.score = scoring.score(row)
+    row['score'] = scoring.score(row)
 
     coll.update({'full_name': row['full_name']}, {'$set': row}, True)  # upsert
     n += 1
-    if n % 10000 == 0:
-      print n, '...'
+    if n % 1000 == 0:
+      print n, '...',
 
   print 'Loaded', n, 'asteroids'
 
@@ -54,4 +57,9 @@ if __name__ == "__main__":
   if len(sys.argv) != 2:
     print 'usage: horizons <fn name>'
     sys.exit(1)
-  locals()[sys.argv[1]]()
+  l = locals()
+  fnname = sys.argv[1]
+  if fnname in l:
+    l[fnname]()
+  else:
+    print 'No such operation "%s"' % fnname
