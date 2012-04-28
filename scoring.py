5ccfdcc83b0d257ca2590632f58d8d3a6afaa0ab
typpo <ian@typpo.us>
2012-04-27 20:09:15 -0400

    scoring tuning

--- scoring.py
+++ scoring.py
@@ -4,21 +4,24 @@
 import math
 
 def closeness_weight(obj):
-  s = (10-obj['moid']) * 3
+  emoid = 1 if isinstance(obj['GM'], basestring) else obj['moid']
+  s = (10-emoid) * 3
   if obj['neo'] != 'N':
-    s = s * 3
-  if obj['pha'] != 'N':
     s = s * 1.4
+  if obj['pha'] != 'N':
+    s = s * 1.2
+
+  #s = s * (1 / obj['q'])    # penalize distance aphelion
   return s
 
 def price(obj):
   G = 6.67300e-11
-  gmass = obj['GM'] if isinstance(obj['GM'], basestring) else 1.47e21 * G
+  gmass = 1.47e21 * G if isinstance(obj['GM'], basestring) else obj['GM']
   radius = obj['diameter'] if 'diameter' in obj else 5  # 5km radius by default
   vol = 4/3 * math.pi * math.pow(radius, 3) # model as sphere
   density = gmass / vol
 
-  return gmass
+  return 10000 * ((1+density) * 500.8 + gmass * 60)
 
 def score(obj):
   return closeness_weight(obj) * price(obj)
