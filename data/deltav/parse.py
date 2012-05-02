9c60627ef6fec8d41e44571c97ba5d80222c6be5
typpo <ian@typpo.us>
2012-05-02 18:29:22 -0400

    parsing delta v

---
+++ data/deltav/parse.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import re
+
+f = open('dv', 'r')
+lines = f.readlines()
+f.close()
+
+r = re.compile('\s*(?P<rank>\d+)\s+(?P<percentile>\d+\.\d+)\s+(?P<name>\(\d+\)(\s+[-\w ]+)?)?\s+(?P<pdes1>\d+)\s+(?P<pdes2>[-\w]+)\s+(?P<deltav>\d+\.\d+)\s+(?P<h>\d+\.\d+)\s+(?P<a>\d+\.\d+)\s+(?P<e>\d+\.\d+)\s+(?P<i>\d+\.\d+)')
+c = 0
+for line in lines:
+  c+=1
+  if c < 4:
+    continue
+
+  m = r.match(line)
+
+  print '%s %s,%s' % (m.group('pdes1'), m.group('pdes2'), m.group('deltav'))
