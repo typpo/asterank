d395499fdda0a9163d4335bc0dfa625625e988d0
typpo <ian@typpo.us>
2012-05-15 17:40:47 -0400

    remove favicon

--- web/app.js
+++ web/app.js
@@ -9,7 +9,6 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 app.use(express.cookieParser());
-app.use(express.favicon(__dirname + '/public/favicon.ico'));
 app.use(express.static(__dirname + '/public'));
 app.use(express.bodyParser());
 
