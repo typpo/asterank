e8337d3a6b2e10a8ca076b0e84661b45e4b96073
Ian <typppo@gmail.com>
2012-05-25 22:21:24 +0800

    more orbit viz work

--- web/public/js/orbit.js
+++ web/public/js/orbit.js
@@ -1,5 +1,5 @@
-var DIAGRAM_HEIGHT = 300, DIAGRAM_WIDTH = 300;
-var SUN_X = DIAGRAM_HEIGHT / 2, SUN_Y = DIAGRAM_WIDTH / 2;
+var DIAGRAM_HEIGHT = 170, DIAGRAM_WIDTH = 300;
+var SUN_X = DIAGRAM_WIDTH / 2, SUN_Y = DIAGRAM_HEIGHT / 2 - 10;
 var DIAGRAM_AU_FACTOR = 50;
 var orbit_svg;
 
@@ -7,11 +7,12 @@ function renderOrbitalDiagram(a, e) {
   $('#orbit-viz').empty();
   orbit_svg = d3.select("#orbit-viz")
       .append("svg:svg")
-      .attr("width", DIAGRAM_HEIGHT)
-      .attr("height", DIAGRAM_WIDTH);
+      .attr("width", DIAGRAM_WIDTH)
+      .attr("height", DIAGRAM_HEIGHT);
 
   plotSun();
   plotEarth();
+  plotVenus();
   plotMars();
 
   plotOrbit(a, e, 'black');
@@ -40,18 +41,29 @@ function plotSun() {
   orbit_svg.append("svg:ellipse")
       .style("stroke", "black")
       .style("fill", "black")
-      .attr("rx", 10)
-      .attr("ry", 10)
+      .attr("rx", 5)
+      .attr("ry", 5)
       .attr("cx", SUN_X)
       .attr("cy", SUN_Y);
 }
 
 function plotEarth() {
   plotOrbit(1.00000011, 0.01671022, 'blue');
+  /*
+  orbit_svg
+      .append('svg:text')
+      .attr("x", 6)
+      .attr("dy", 30)
+      .text(function(d) { return 'yo mama'; })
+      */
 }
 
 function plotMars() {
   plotOrbit(1.52366231, 0.0935, 'red');
+}
+
+function plotVenus() {
+  plotOrbit(0.72333199, 0.00677323, 'orange');
 }
 
 function plotCeres() {
