2fe78cfee83e3c7e19430cddd3bbcf560a7a3618
Ian <typppo@gmail.com>
2012-07-22 17:16:04 -0700

    work on orbits and converting everything to radians, using ecliptic as 0,0,0 plane

--- web/public/3d/js/ellipse.js
+++ web/public/3d/js/ellipse.js
@@ -29,8 +29,8 @@
     line.position.set(0,0,0);
 
     line.rotation.x = pi/2;
+    line.rotation.y = (this.eph.i * pi / 180) - (Ephemeris.earth.i * pi/180);
     line.rotation.z = this.eph.w * pi / 180;
-    line.rotation.y = this.eph.i * pi / 180;
 
     this.object3D = line;
   }
@@ -39,6 +39,7 @@
     // http://www.davidcolarusso.com/astro/
     // http://www.stargazing.net/kepler/ellipse.html#twig02a
 
+    console.log(this.eph.name);
     //var M = this.eph.M;
     var e = this.eph.e;
     var a = this.eph.a;
@@ -46,28 +47,38 @@
     var o = this.eph.O; // longitude of ascending node
     var p = this.eph.w; // longitude of perihelion
     var L = this.eph.L; // mean longitude
-    var M =  L - p; // mean anomaly on date of elements
+    var M =  pi/180 * (L - p); // mean anomaly on date of elements
+    M = (M + pi) / 2*pi;
+    M = (M - Math.floor(M)) * 2*pi - pi; // modulo 180
+    //M = 180/pi * (2 * pi) / Math.pow(a, 3/2);
+    //console.log('M2', M);
 
     var sin = Math.sin, cos = Math.cos;
 
-    // true anomaly
+    // true anomaly approximation, using Equation of Center
+    M=254.895962*pi/180;
+    e = 0.0934231
     var v = M + 180/pi * ( (2 * e - e*e*e/4) * sin(M)
-                                 + 5/4 * e*e * sin(2*M)
-                                 + 13/12 * e*e*e * sin(3*M));
-    console.log(v);
+         + 5/4 * e*e * sin(2*M)
+         + 13/12 * e*e*e * sin(3*M));
+    console.log('M=', M, 'v=', v);
+    console.log(v*pi/180, '=?', 2*e*sin(M*pi/180)*60);
 
     // radius vector, in AU
-    var r = a * (1 - e*e) / (1 + e * cos(v)) * PIXELS_PER_AU;
+    var r = a * (1 - e*e) / (1 + e * cos(v));
 
     // heliocentric coords
-    var X = r * [cos(o) * cos(v + p - o) - sin(o) * sin(v + p - o) *
-    cos(i)]
-    var Y = r * [sin(o) * cos(v + p - o) + cos(o) * sin(v + p - o) *
-    cos(i)]
-    var Z = r * [sin(v + p - o) * sin(i)]
+    var X = r * (cos(o) * cos(v + p - o) - sin(o) * sin(v + p - o) * cos(i))
+    var Y = r * (sin(o) * cos(v + p - o) + cos(o) * sin(v + p - o) * cos(i))
+    var Z = r * (sin(v + p - o) * sin(i))
+
+    var x = X;
+    var y = Z //Y * cos(i) - Z * sin(i);
+    var z = Y //Y * sin(i) + Z * cos(i);
 
     //console.log(X, Y, Z);
 
+    /*
     var material = new THREE.ParticleCanvasMaterial({
       color: this.opts.color,
       program: function (context) {
@@ -78,12 +89,20 @@
       }
     });
     var particle = new THREE.Particle(material);
-    particle.position.x = X;
-    particle.position.y = Y;
-    particle.position.z = Z;
-    //particle.rotation.x = pi/2;
-    //particle.rotation.z = this.eph.w * pi / 180;
-    //particle.rotation.y = eph.i * pi / 180;
+    */
+    var geometry= new THREE.SphereGeometry(1);
+    var material= new THREE.MeshBasicMaterial({color: this.opts.color});
+    var particle = new THREE.Mesh(geometry, material);
+    particle.position.x = x;
+    particle.position.y = y;
+    particle.position.z = z;
+    particle.position.multiplyScalar(PIXELS_PER_AU);
+
+    /*
+    particle.rotation.x = pi/2;
+    particle.rotation.z = this.eph.w * pi / 180;
+    particle.rotation.y = this.eph.i * pi / 180;
+    */
 
     this.particle = particle;
   }
