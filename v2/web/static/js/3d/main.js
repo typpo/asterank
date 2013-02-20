13867c22185b5e759455cf237e330c2d927a0023
Ian <typppo@gmail.com>
2013-02-19 22:22:10 -0800

    disable fullscreen api

--- v2/web/static/js/3d/main.js
+++ v2/web/static/js/3d/main.js
@@ -201,9 +201,11 @@ function Asterank3D(container) {
     //THREE.Object3D._threexDomEvent.camera(camera);    // camera mouse handler
     THREEx.WindowResize(renderer, camera, container);    // handle window resize
     // Fullscreen api
+    /*
     if (THREEx.FullScreen.available()) {
       THREEx.FullScreen.bindKey();
     }
+    */
 
     scene.add(camera);
 
