01927f04fdefa2a89892b841418d725e744c3521
Ian Webster <typppo@gmail.com>
2013-03-10 19:07:47 -0700

    galaxies alias and minor script changes

--- scripts/clear-nginx-cache.sh
+++ scripts/clear-nginx-cache.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo grep -lr "$1" /var/www/cache | xargs grep KEY:
+sudo grep -lr "$1" /var/www/cache | sudo xargs grep -a KEY:
 
-read -p "Deleting these. OK?" -n 1 -r
+read -p "Deleting these. OK [y/N]? " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   sudo grep -lr "$1" /var/www/cache | sudo xargs rm
