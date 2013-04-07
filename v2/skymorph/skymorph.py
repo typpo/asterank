a20253ce46089275b45c71e8183d43b11c7da28d
Ian <typppo@gmail.com>
2013-04-07 14:14:18 -0700

    redis cache backing skymorph api

--- v2/skymorph/skymorph.py
+++ v2/skymorph/skymorph.py
@@ -1,5 +1,8 @@
 import re
+import urlparse
 import requests
+import json
+from redis import StrictRedis
 from bs4 import BeautifulSoup
 
 ##### Constants
@@ -21,13 +24,24 @@ URL_BASE = 'http://skyview.gsfc.nasa.gov/'
 # The html is so bad that BeautifulSoup won't work
 IMAGE_PARSING_REGEX = re.compile("img src='(.*?)'")
 
+##### Redis Config
+REDIS_PREFIX = 'skymorph'
+redis = StrictRedis(host='localhost', port=6379, db=3)
+
 ##### Functions
 
 def search_target(target):
-  # TODO cache
+  target = target.upper()
+  redis_key = '%s:target:%s' % (REDIS_PREFIX, target)
 
-  r = requests.get(SEARCH_TARGET_URL % (target))
-  return parse_results_table(r.text)
+  cached = redis.get(redis_key)
+  if cached:
+    return json.loads(cached)
+  else:
+    r = requests.get(SEARCH_TARGET_URL % (target))
+    results = parse_results_table(r.text)
+    redis.set(redis_key, json.dumps(results))
+    return results
 
 def parse_results_table(text):
   soup = BeautifulSoup(text)
@@ -55,7 +69,16 @@ def parse_results_table(text):
   return entries
 
 def get_images(target, key):
-  # TODO cache
+  redis_key = '%s:images:%s' % (REDIS_PREFIX, key)
+  cached = redis.get(redis_key)
+  if cached:
+    # TODO cache in s3.  Right now we are caching temporary urls,
+    # so need to check if they're still valid
+    json_obj = json.loads(cached)
+    r = requests.head(json_obj['url'])
+    if r.status_code == 200:
+      return json_obj
+
   params = {
       'Headers_NEAT': '|Observation|Time|ObjRA|ObjDec|Plt RA|Plt Dec|Magnitude|V_RA|V_Dec|E_Maj|E_Min|E_PosAng|x|y|',
       'Check_NEAT': key,
@@ -69,9 +92,14 @@ def get_images(target, key):
   r = requests.post(IMAGE_QUERY_URL, params=params)
   matches = IMAGE_PARSING_REGEX.search(r.text)
   if matches is None:
-    return ''
-  return matches.group(1)
+    return {'success': False}
+
+  result = {'success': True, 'url': urlparse.urljoin(URL_BASE, matches.group(1))}
+  redis.set(redis_key, json.dumps(result))
+  return result
 
 if __name__ == "__main__":
   #result = search_target('J99TS7A')
+  #get_images('', '|001204124410|51882.530787037|129.062741402712|4.64001695570385|128.337645|4.0726|20.70|-4.28|-11.08|n.a.|n.a.|n.a.|71.9154214757038|547.287989060186|y|')
+
   pass
