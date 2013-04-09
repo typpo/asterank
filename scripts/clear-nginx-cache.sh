840dfb32ac3f58dff928bc1eac34aedc4654fe9f
Ian Webster <typppo@gmail.com>
2013-04-09 10:47:37 -0700

    update planned improvements

--- scripts/clear-nginx-cache.sh
+++ scripts/clear-nginx-cache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo grep -lr "$1" /var/www/cache | sudo xargs grep -a KEY:
+sudo grep -lr "$1" /var/www/cache | xargs grep -a KEY:
 
 read -p "Deleting these. OK [y/N]? " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
