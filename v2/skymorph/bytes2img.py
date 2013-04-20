aea69ca9e8383b9a2f6f757628c63ee31182272b
Ian <typppo@gmail.com>
2013-04-20 12:46:43 -0700

    increase contrast

--- v2/skymorph/bytes2img.py
+++ v2/skymorph/bytes2img.py
@@ -20,13 +20,16 @@ start_x = header_vals[0]
 start_y = header_vals[1]
 end_x = header_vals[2]
 end_y = header_vals[3]
-width = end_x - start_x
-height = end_y - start_y
-bytes_per_pixel = header_vals[4] / 8
+width = end_x# - start_x
+height = end_y# - start_y
+bits_per_pixel = header_vals[4]
+bit_range = 2 ** bits_per_pixel
+bytes_per_pixel = bits_per_pixel / 8
 
 print 'Parsing %dx%d file at %d bytes per pixel' % (width, height, bytes_per_pixel)
 
 rows = []
+last_colored_int = 0
 for y in range(height):
   row = []
   for x in range(width):
@@ -36,6 +39,12 @@ for y in range(height):
       break
     hexstr = ''.join(["{0:x}".format(ord(b)) for b in pixel])
     intval = int(hexstr, 16)
+    if intval <= 4000:
+      intval = last_colored_int
+    else:
+      last_colored_int = intval
+    if abs(intval - bit_range / 2) < 1000:
+      intval = 0
     row.append(intval)
   rows.append(row)
 
