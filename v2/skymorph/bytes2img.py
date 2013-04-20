b5d677ee2e2bcacdae1279880c9faf194c00aa1e
Ian <typppo@gmail.com>
2013-04-20 13:42:53 -0700

    make skymorph direct connect a module

--- v2/skymorph/bytes2img.py
+++ v2/skymorph/bytes2img.py
@@ -1,58 +1,85 @@
 import png
+import os
 import sys
+import urllib2
+import tempfile
 
-if len(sys.argv) > 1:
-  FILE = sys.argv[1]
-else:
-  FILE = 'extract_subset.pl'
+BASE_URL = 'http://kaspar.jpl.nasa.gov/cgi-bin'
+ENDPOINT = '/extract_subset.pl'
+QUERY_FORMAT = '?Id=%s&X0=%d&Y0=%d&Nx=%d&Ny=%d'
 
-f = open(FILE, 'rb')
-header_vals = []
-for c in range(5):
-  bytes = f.read(4)
-  hexstr = ''.join(["{0:x}".format(ord(b)) for b in bytes])
-  intval = int(hexstr, 16)
-  print '%s (%d)' % (hexstr, intval)
+def load_url(id, x0, y0, width, height):
+  formatted_query = QUERY_FORMAT % (id, x0, y0, width, height)
+  URL = '%s%s%s' % (BASE_URL, ENDPOINT, formatted_query)
+  req = urllib2.urlopen(URL)
+  CHUNK = 16 * 1024
+  file = tempfile.NamedTemporaryFile(delete=False)
+  with file as fp:
+    while True:
+      chunk = req.read(CHUNK)
+      if not chunk: break
+      fp.write(chunk)
+  return os.path.abspath(file.name)
 
-  header_vals.append(intval)
+def process_from_internet(id, x0, y0, width, height, output_path):
+  print 'Loading url...'
+  path = load_url(id, x0, y0, width, height)
+  print 'Building image...'
+  process(path, output_path)
+  os.remove(path)
 
-start_x = header_vals[0]
-start_y = header_vals[1]
-end_x = header_vals[2]
-end_y = header_vals[3]
-width = end_x# - start_x
-height = end_y# - start_y
-bits_per_pixel = header_vals[4]
-bit_range = 2 ** bits_per_pixel
-bytes_per_pixel = bits_per_pixel / 8
-
-print 'Parsing %dx%d file at %d bytes per pixel' % (width, height, bytes_per_pixel)
-
-rows = []
-last_colored_int = 0
-for y in range(height):
-  row = []
-  for x in range(width):
-    pixel = f.read(bytes_per_pixel)
-    if pixel == '':
-      print 'EOF before expected'
-      break
-    hexstr = ''.join(["{0:x}".format(ord(b)) for b in pixel])
+def process(path, output_path):
+  f = open(path, 'rb')
+  header_vals = []
+  for c in range(5):
+    bytes = f.read(4)
+    hexstr = ''.join(["{0:x}".format(ord(b)) for b in bytes])
     intval = int(hexstr, 16)
-    if intval <= 4000:
-      intval = last_colored_int
-    else:
-      last_colored_int = intval
-    if abs(intval - bit_range / 2) < 1000:
-      # increase contrast
-      intval = 0
-    row.append(intval)
-  rows.append(row)
+    header_vals.append(intval)
 
-f.close()  # close input
+  start_x = header_vals[0]
+  start_y = header_vals[1]
+  end_x = header_vals[2]
+  end_y = header_vals[3]
+  width = end_x# - start_x
+  height = end_y# - start_y
+  bits_per_pixel = header_vals[4]
+  bit_range = 2 ** bits_per_pixel
+  bytes_per_pixel = bits_per_pixel / 8
+
+  print 'Parsing %dx%d file at %d bytes per pixel' % (width, height, bytes_per_pixel)
+
+  rows = []
+  last_colored_int = 0
+  for y in range(height):
+    row = []
+    for x in range(width):
+      pixel = f.read(bytes_per_pixel)
+      if pixel == '':
+        print 'EOF before expected'
+        break
+      hexstr = ''.join(["{0:x}".format(ord(b)) for b in pixel])
+      intval = int(hexstr, 16)
+      if intval <= 4000:
+        intval = last_colored_int
+      else:
+        last_colored_int = intval
+      if abs(intval - bit_range / 2) < 1000:
+        # increase contrast
+        intval = 0
+      row.append(intval)
+    rows.append(row)
+
+  f.close()  # close input
 
 # Write output
-f = open('output.png', 'wb')
-w = png.Writer(width, height, greyscale=True, bytes_per_sample=bytes_per_pixel)
-w.write(f, rows)
-f.close()
+  f = open(output_path, 'wb')
+  w = png.Writer(width, height, greyscale=True, bytes_per_sample=bytes_per_pixel)
+  w.write(f, rows)
+  f.close()
+
+if __name__ == "__main__":
+  if len(sys.argv) > 2:
+    process(sys.argv[1], sys.argv[2])
+  else:
+    process_from_internet('001204131418', 0, 300, 500, 500, 'output.png')
