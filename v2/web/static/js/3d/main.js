809e0e5de581abfaeee10daddc25c250fb1e97f2
Ian <typppo@gmail.com>
2013-04-27 12:17:15 -0700

    reenable webgl

--- v2/web/static/js/3d/main.js
+++ v2/web/static/js/3d/main.js
@@ -28,7 +28,7 @@
   })();
 
   /** Constants **/
-  var WEB_GL_ENABLED = false;
+  var WEB_GL_ENABLED = true;
 
   var MAX_NUM_ORBITS = 4000;
   var CANVAS_NUM_ORBITS = 15;  // gimped version orbits
