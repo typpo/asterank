495eba150e8e99785036b43048d4aee6885237f7
Ian Webster <typppo@gmail.com>
2013-05-29 12:39:03 -0700

    Fix MPC pipeline (fixes #51)

--- data/pipeline/run/50_mpc/run.sh
+++ data/pipeline/run/50_mpc/run.sh
@@ -6,8 +6,8 @@ OUT="`git rev-parse --show-toplevel`/data/pipeline/out"
 STATIC="`git rev-parse --show-toplevel`/data/pipeline/static"
 mkdir -p $OUT/mpc
 
-$GZ_PATH=$OUT/mpc/mpcorb.dat.gz
-$OUT_PATH=$OUT/mpc/mpcorb.dat
+GZ_PATH=$OUT/mpc/mpcorb.dat.gz
+OUT_PATH=$OUT/mpc/mpcorb.dat
 
 wget -O $GZ_PATH http://www.minorplanetcenter.net/iau/MPCORB/MPCORB.DAT.gz
 gunzip -c $GZ_PATH > $OUT_PATH
