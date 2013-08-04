5687abd26439457957a2d173bb57253c4d595161
Ian <typppo@gmail.com>
2013-08-03 21:47:13 -0700

    darkening reds in sdss

--- v2/sdss/split.py
+++ v2/sdss/split.py
@@ -4,16 +4,22 @@
 #
 import sys
 import os
-from PIL import Image
+from PIL import Image, ImageEnhance
 
 path = os.path.abspath(sys.argv[1])
 newpath_prefix, ext = os.path.splitext(path)
 
 print 'Splitting %s...' % path
 
+#def enhance(img):
+#  enhancer = ImageEnhance.Contrast(img)
+#  return enhancer.enhance(20)
+
 im = Image.open(path)
 im.load()
 r, g, b = im.split()
+
+r = r.point(lambda p: p * 0.85)   # reds tend to overpower in sdss imagery
 
 r.save('%s_r_split%s' % (newpath_prefix, ext))
 g.save('%s_g_split%s' % (newpath_prefix, ext))
