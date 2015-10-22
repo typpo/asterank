546c3117047fad58d41e3ea485d8f68bdc1fa3a5
Ian Webster <ianw_git@ianww.com>
2015-10-21 23:08:39 -0700

    earlier close pass

--- static/js/3d/main.js
+++ static/js/3d/main.js
@@ -85,9 +85,11 @@
 
   // 2015 TB145 feature special case
   if (featured_2015_tb145) {
-    jed = toJED(new Date('2015-05-01'));
+    jed = toJED(new Date('2015-01-01'));
     if (typeof mixpanel !== 'undefined') mixpanel.track('2015_tb145 special');
-    $('#hide_sidebar').trigger('click');
+    setTimeout(function() {
+      $('#hide_sidebar').trigger('click');
+    }, 0);
   }
 
   function initGUI() {
